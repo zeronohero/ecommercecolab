b3b47d5886ae559a8ae56bacda60ef5160e73c7e
MarcelloCV <alithepistimonikos@gmail.com>
2024-02-23 00:53:42 +0700

    Trying to commit and push changes to a project repo for the second time

--- Main/App.java
+++ Main/App.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class App {
     //ini gw Marcellow
+    //djdnd
  public static void main(String[] args) {
     Scanner scan = new Scanner (System.in);
     System.out.print("enter your name");
