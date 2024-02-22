9065712087e561c431a83ffd98e8ec89ce9a3933
zeronohero <159472537+zeronohero@users.noreply.github.com>
2024-02-22 20:16:50 +0700

    adlfkjadsflj

--- Main/App.java
+++ Main/App.java
@@ -1,12 +1,8 @@
-package Main;
 import java.util.Scanner;
-/**
- * App
- */
 public class App {
  public static void main(String[] args) {
     Scanner scan = new Scanner (System.in);
-    System.out.println("enter your name");
+    System.out.print("enter your name");
     String hello = scan.nextLine();
     System.out.println(hello);
  }
