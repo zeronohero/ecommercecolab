bde2e06c58f10bf19547839fa778eda1a8b90277
zeronohero <159472537+zeronohero@users.noreply.github.com>
2024-02-23 22:16:24 +0700

    Co-authored-by: GuestySkuy <GuestySkuy@users.noreply.github.com>

--- Main/App.java
+++ Main/App.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 public class App {
-    //ini gw Marcellow
-    //djdnd
  public static void main(String[] args) {
     Scanner scan = new Scanner (System.in);
     System.out.print("enter your name");
     String hello = scan.nextLine();
     System.out.println(hello);
-    System.out.print("hallo")
-    
- }
+    System.out.print("hallo");
+    System.out.print("hello world 2")
+ } 
     
 }
